63e55c6c26e920d63a0bf085ff5ecd3593454b10
Liang Zhang <zhangliangnuaa@gmail.com>
2017-12-20 07:25:51 +0000

    Add new ejs and route

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.get("/", function(req, res) {
     res.redirect("/blogs");
 });
 
+// INDEX ROUTE
 app.get("/blogs", function (req, res) {
     Blog.find({}, function(err, blogs){
         if(err){
@@ -40,6 +41,25 @@ app.get("/blogs", function (req, res) {
     });
 });
 
+// NEW ROUTE
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+})
+
+// CREATE ROUTE
+app.post("/blogs", function (req, res) {
+    // console.log(req.body);
+    // console.log("===========")
+    // console.log(req.body);
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        } else {
+            //then, redirect to the index
+            res.redirect("/blogs");
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("BLOG APP SERVER IS RUNNING!");
